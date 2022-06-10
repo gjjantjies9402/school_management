775477fcd1d98c5cb14f052d0a17a708943b25a0
katia101 <217149073@mycput.ac.za>
2022-06-10 20:41:18 +0200

    First domain commit

---
+++ src/main/java/za/ac/cput/Domain/studentAddress.java
@@ -0,0 +1,22 @@
+/**
+ * Name: Mandisa Msongelwa
+ * Student number: 217149073
+ * Group 28
+ * Question 1 (Domain Package)
+ * Date: 10/06/2022
+ */
+
+package za.ac.cput.Domain;
+import za.ac.cput.Domain.student;
+
+public class studentAddress {
+
+    private String studentId;
+
+
+    private studentAddress(){}
+
+    //public studentAddress(Builder builder) {}
+
+    public String getStudentId() {return studentId;}
+}

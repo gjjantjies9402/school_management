03d3189df18c712024475737ef928b57d9c67939
katia101 <217149073@mycput.ac.za>
2022-06-10 23:13:16 +0200

    Last domain commit

--- src/main/java/za/ac/cput/Domain/StudentAddress.java
+++ src/main/java/za/ac/cput/Domain/StudentAddress.java
@@ -3,18 +3,57 @@
  * Student number: 217149073
  * Group 28
  * Question 1 (Domain Package)
+ * StudentAddress.java
  * Date: 10/06/2022
  */
 
 package za.ac.cput.Domain;
 
+
 public class StudentAddress {
 
     private String studentId;
-
+    private Address address;
 
     private StudentAddress(){}
 
+    public StudentAddress(Builder builder) {
+        this.studentId = builder.studentId;
+        this.address = builder.address;
+    }
 
     public String getStudentId() {return studentId;}
+
+    public Address getAddress() {return address;}
+
+    @Override
+    public String toString() {
+        return "Student ID{" + studentId +
+                "}, Address{" + address +
+                "}";
+    }
+
+    public static class Builder {
+        private String studentId;
+        private Address address;
+
+        public Builder studentId(String studentId) {
+            this.studentId = studentId;
+            return this;
+        }
+
+        public Builder address(Address address) {
+            this.address = address;
+            return this;
+        }
+
+        public Builder copy(StudentAddress studentAddress) {
+            this.studentId = studentAddress.studentId;
+            this.address = studentAddress.address;
+
+            return this;
+        }
+
+        public StudentAddress build() {return new StudentAddress(this);}
+    }
 }

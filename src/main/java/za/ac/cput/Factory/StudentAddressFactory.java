2483b6e20bb768429e2f4041aaa5ce139831cb7e
katia101 <217149073@mycput.ac.za>
2022-06-10 23:38:08 +0200

    Last (final) domain commit

---
+++ src/main/java/za/ac/cput/Factory/StudentAddressFactory.java
@@ -0,0 +1,13 @@
+/**
+ * Name: Mandisa Msongelwa
+ * Student number: 217149073
+ * Group 28
+ * Question 2a (Factory Package)
+ * StudentAddressFactory.java
+ * Date: 10/06/2022
+ */
+
+package za.ac.cput.Factory;
+
+public class StudentAddressFactory {
+}
---
+++ src/main/java/za/ac/cput/Factory/StudentFactory.java
@@ -0,0 +1,17 @@
+/**
+ * Name: Mandisa Msongelwa
+ * Student number: 217149073
+ * Group 28
+ * Question 2a (Factory Package)
+ * StudentFactory.java
+ * Date: 10/06/2022
+ */
+
+package za.ac.cput.Factory;
+
+import za.ac.cput.Domain.Student;
+
+public class StudentFactory {
+
+    //public static Student buildStudent()
+}

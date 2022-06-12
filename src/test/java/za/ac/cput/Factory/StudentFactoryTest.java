bcb0cefe03253fb385fd30223c57ce75b735224c
katia101 <217149073@mycput.ac.za>
2022-06-12 14:24:10 +0200

    Initial Factory Tests commit

---
+++ src/test/java/za/ac/cput/Factory/StudentFactoryTest.java
@@ -0,0 +1,17 @@
+package za.ac.cput.Factory;
+import org.junit.jupiter.api.Test;
+import za.ac.cput.Domain.Student;
+import za.ac.cput.Domain.Name;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentFactoryTest {
+
+    @Test
+    void buildStudent() {
+        Student student = StudentFactory.buildStudent("Mso101", "mso101@mycput.ac.za", NameFactory.createName("Buko", "BK", "Ramncwana"));
+        assertNotNull(student);
+        System.out.println("Student: " + student);
+
+    }
+}

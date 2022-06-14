ea9c8ef4a802f20a0049f3d809c05c77c57ef0bf
katia101 <217149073@mycput.ac.za>
2022-06-14 19:09:13 +0200

    Recent changes v3

--- src/main/java/za/ac/cput/Services/Interfaces/IStudentService.java
+++ src/main/java/za/ac/cput/Services/Interfaces/IStudentService.java
@@ -1,6 +1,6 @@
-//package za.ac.cput.Services.Interfaces;
-//
-//import za.ac.cput.Domain.Student;
-//
-//public interface IStudentService extends IService <Student, String> {
-//}
+package za.ac.cput.Services.Interfaces;
+
+import za.ac.cput.Domain.Student;
+
+public interface IStudentService extends IService <Student, String> {
+}

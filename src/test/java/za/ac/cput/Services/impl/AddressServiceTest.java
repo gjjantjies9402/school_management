db1ac849e18b4ee0c47253e51867502bae94aa63
Gaynor Jantjies <gjjantjies@gmail.com>
2022-06-16 17:30:11 +0200

    services

---
+++ src/test/java/za/ac/cput/Services/impl/AddressServiceTest.java
@@ -0,0 +1,24 @@
+package za.ac.cput.Services.impl;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AddressServiceTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void read() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+}
